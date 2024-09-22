e7c6809836f162a3a8927307d2b82e18c287b351
M Prithvi <madivadapr@gmail.com>
2024-09-22 20:18:43 +0530

    My 1st MiniGFG Project

---
+++ src/main/java/mypkg/MyCalc.java
@@ -0,0 +1,23 @@
+package mypkg;
+
+public class MyCalc {
+	
+	public int sum(int a,int b) {
+		return a+b;
+	}
+	
+	public int diff(int a,int b) {
+		return a-b;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		MyCalc obj=new MyCalc();
+		System.out.println("Sum is: "+obj.sum(20, 10));
+		System.out.println("Diff is: "+obj.diff(20, 10));
+		
+
+	}
+
+}
