1e2e14f4f2d5ddde4bf1e709f027687cd86f97de
M Prithvi <madivadapr@gmail.com>
2024-09-22 20:53:55 +0530

    mul & div added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -10,6 +10,14 @@ public class MyCalc {
 		return a-b;
 	}
 	
+	public int mul(int a,int b) {
+		return a*b;
+	}
+	
+	public int div(int a,int b) {
+		return a/b;
+	}
+	
 	
 
 	public static void main(String[] args) {
